4e15170050065f4fba392605578c961e4975ca6f
Pierre Bretéché <pbreteche@dawan.fr>
2024-09-17 10:24:13 +0200

    find by parameters, pagination

--- src/Controller/GrumpyPizzaController.php
+++ src/Controller/GrumpyPizzaController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\GrumpyPizzaRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -12,9 +13,17 @@ class GrumpyPizzaController extends AbstractController
 {
     #[Route]
     public function index(
+        Request $request,
         GrumpyPizzaRepository $repository,
     ): Response {
-        $pizzas = $repository->findAll();
+        try {
+            $page = $request->query->getInt('page', 1);
+        } catch (\Throwable) {
+            return $this->redirectToRoute($request->attributes->get('_route'));
+        }
+        $limitPerPage = 10;
+        $offset = $limitPerPage * (max($page, 1) - 1);
+        $pizzas = $repository->findBy([], ['name' => 'ASC'], $limitPerPage, $offset);
 
         return $this->render('grumpy_pizza/index.html.twig', [
             'pizzas' => $pizzas,
